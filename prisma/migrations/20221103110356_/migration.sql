ec133a1962a1039345f1f4097450bc40efef1e37
Karel <karel789@hotmail.com>
2022-11-03 19:05:22 +0800

    added prisma db

---
+++ prisma/migrations/20221103110356_/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Template" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "public" BOOLEAN NOT NULL DEFAULT false,
+    "template" TEXT NOT NULL,
+
+    CONSTRAINT "Template_pkey" PRIMARY KEY ("id")
+);
