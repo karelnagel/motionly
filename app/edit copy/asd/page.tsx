11f397f8afe3c8e10bb6173b20708b648b3673c0
Karel <karel789@hotmail.com>
2022-11-04 20:24:00 +0800

    test

---
+++ app/edit copy/asd/page.tsx
@@ -0,0 +1,24 @@
+import Edit from "./Edit";
+import { PrismaClient } from "@prisma/client";
+
+export default async function Page({ params: { id } }: any) {
+  const template = await new PrismaClient().template.findUnique({ where: { id } });
+  if (!template) return <div>No template with this id!</div>;
+
+  return (
+    <div>
+      <Edit
+        template={
+          {
+            width: template.width,
+            height: template.height,
+            elements: template.elements,
+            id: template.id,
+            name: template.name,
+            description: template.description,
+          } as any
+        }
+      />
+    </div>
+  );
+}
