41710a9db0f6475ad803a1e71cab83a597e98954
Karel <karel789@hotmail.com>
2022-11-05 12:50:41 +0800

    test3

--- app/asd/[id]/page.tsx
+++ app/asd/[id]/page.tsx
@@ -1,15 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
 export default async function Page({ params: { id } }: any) {
-  const template = await new PrismaClient().template.findUnique({ where: { id } });
-  if (!template) return <div>No template with this id!</div>;
+  const templates = await new PrismaClient().template.findMany();
+
+  if (!templates || templates.length > 0) return <div>No template with this id!</div>;
 
   return (
     <div>
       <p>{id}</p>
-      <p>{template.name}</p>
-      <p>{template.width}</p>
-      
+      <p>{templates[0].name}</p>
+      <p>{templates[0].width}</p>
     </div>
   );
 }
