cdde883dbf2c092630055dca12394d7c6e323d0b
Karel <karel789@hotmail.com>
2022-11-05 13:04:30 +0800

    gasdfasdf

--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -10,15 +10,11 @@ export default async function Templates() {
         {templates.map((template) => (
           <Link
             key={template.id}
-            href={`/edit/${template.id}`}
+            href={`/edit?id=${template.id}`}
             className=" relative rounded-lg overflow-hidden h-40 w-60"
           >
             {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src={`/api/images/${template.id}`}
-              alt={template.name}
-              className="cover"
-            />
+            <img src={`/api/images/${template.id}`} alt={template.name} className="cover" />
             <div className="absolute bottom-0 w-full left-0 py-2 text-center bg-black text-white bg-opacity-50">
               {template.name}
             </div>
