119a13f6f283758737e2d567874024c71d725bf4
Karel <karel789@hotmail.com>
2022-11-06 23:27:23 +0800

    lil bug fix

--- pages/api/images/[id].tsx
+++ pages/api/images/[id].tsx
@@ -11,9 +11,10 @@ export default async function handler(req: NextRequest, res: NextApiResponse) {
   const id = req.nextUrl.searchParams.get("id");
   const modifiedElements = req.nextUrl.searchParams.get("elements");
   if (!id) return res.status(403).end();
+
   const result = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/templates/${id}`);
   const { width, height, elements } = await result.json();
-  const finalElem = [...JSON.parse(elements), ...JSON.parse(modifiedElements || "{}")];
+  const finalElem = [...JSON.parse(elements), ...JSON.parse(modifiedElements || "[]")];
   return new ImageResponse(
     <Template width={width} height={height} elements={finalElem} setElements={() => {}} />,
     {
