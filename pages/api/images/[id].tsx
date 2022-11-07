5bdeac3ec90b439892535beb262c3ea6a08a4cda
Karel <karel789@hotmail.com>
2022-11-07 17:53:30 +0800

    added mod width and height

--- pages/api/images/[id].tsx
+++ pages/api/images/[id].tsx
@@ -9,22 +9,24 @@ export const config = {
 };
 
 export default async function handler(req: NextRequest, res: NextApiResponse) {
-  const id = req.nextUrl.searchParams.get("id");
+  const params = req.nextUrl.searchParams;
+  const id = params.get("id");
   if (!id) return res.status(403).end();
 
-  let modifiedElements: ElementType[] = [];
+  let modElements: ElementType[] = [];
+  const modWidth = Number(params.get("width"));
+  const modHeight = Number(params.get("height"));
   try {
-    modifiedElements = JSON.parse(req.nextUrl.searchParams.get("elements") || "[]");
+    modElements = JSON.parse(params.get("elements") || "[]");
   } catch (e) {
     console.log(e);
   }
-
   const result = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/templates/${id}`);
   const { width, height, elements } = await result.json();
   const replace = (elems: ElementType[]) => {
     const newElems: ElementType[] = [];
     for (let element of elems) {
-      const mod = modifiedElements.find((e) => e.id === element.id);
+      const mod = modElements.find((e) => e.id === element.id);
       if (mod) element = { ...element, ...mod };
 
       if (element.type === "div") {
@@ -39,8 +41,8 @@ export default async function handler(req: NextRequest, res: NextApiResponse) {
   return new ImageResponse(
     <Template width={width} height={height} elements={finalElems} setElements={() => {}} />,
     {
-      width: width,
-      height: height,
+      width: modWidth || width,
+      height: modHeight || height,
     }
   );
 }
