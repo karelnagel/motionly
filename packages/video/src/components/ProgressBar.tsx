8836742b5f9ce99b371be2c0dae7dc700e349539
Karel <karel789@hotmail.com>
2022-12-12 20:15:28 +0800

    added circle and spotify progressbar

--- packages/video/src/components/ProgressBar.tsx
+++ packages/video/src/components/ProgressBar.tsx
@@ -6,9 +6,12 @@ export const ProgressBarComp = ({
   color,
   barWidth,
   backgroundColor,
+  height,
+  width,
 }: ProgressBarCompProps) => {
   const frame = useCurrentFrame();
   const { durationInFrames } = useVideoConfig();
+  const progress = (frame / durationInFrames) * 100;
   if (progressBarType === "line")
     return (
       <div style={{ width: "100%", height: "100%", position: "relative", backgroundColor }}>
@@ -19,10 +22,70 @@ export const ProgressBarComp = ({
             left: 0,
             height: "100%",
             backgroundColor: color,
-            width: `${(frame / durationInFrames) * 100}%`,
+            width: `${progress}%`,
           }}
         />
       </div>
+    );
+  if (progressBarType === "spotify")
+    return (
+      <div
+        style={{
+          width: "100%",
+          height,
+          display: "flex",
+          alignItems: "center",
+          position: "relative",
+          margin: `0 ${height / 2}px`,
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            width: "100%",
+            height: height / 2,
+            borderRadius: height / 4,
+            left: 0,
+            backgroundColor,
+            overflow: "hidden",
+          }}
+        >
+          <div
+            style={{
+              width: `${progress}%`,
+              background: color,
+              position: "absolute",
+              left: 0,
+              height: "100%",
+            }}
+          />
+        </div>
+        <div
+          style={{
+            borderRadius: "100%",
+            height,
+            position: "absolute",
+            width: height,
+            background: color,
+            left: `${progress}%`,
+            transform: "translate(-50%, 0)",
+          }}
+        />
+      </div>
+    );
+  if (progressBarType === "circle")
+    // Todo
+    return (
+      <svg viewBox={`0 0 ${width} ${height}`}>
+        <circle
+          cx="50%"
+          cy="50%"
+          r={`calc(50% - ${barWidth! / 2}px)`}
+          stroke={backgroundColor}
+          strokeWidth={barWidth}
+          fill="transparent"
+        />
+      </svg>
     );
   return null;
 };
--- packages/video/src/compositions/progressbars.ts
+++ packages/video/src/compositions/progressbars.ts
@@ -2,18 +2,18 @@ import { CompProps } from "@asius/types";
 import { baseComp, videoUrl } from "./consts";
 
 const base: CompProps = {
-    ...baseComp, type: "progressbar", progressBarType: "line", barWidth: 10, color: "red", backgroundColor: "pink"
+    ...baseComp, type: "progressbar", height: 100, width: 1000, progressBarType: "line", barWidth: 20, color: "red", backgroundColor: "pink"
 }
 
 export const progressbars: CompProps[] = [
     {
-        ...base, id: "line", height: 100
+        ...base, id: "line"
     },
     {
         ...base, id: "spotify", progressBarType: "spotify"
     },
     {
-        ...base, id: "circle", progressBarType: "circle"
+        ...base, id: "circle", progressBarType: "circle", height: 400, width: 400
     },
     {
         ...base, id: "square", progressBarType: "square"
