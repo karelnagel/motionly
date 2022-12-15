2a04313e996c863afad306ffb4b3e30c7dd326f3
Karel <karel789@hotmail.com>
2022-12-15 12:01:31 +0800

    login page

--- packages/client/app/Login.tsx
+++ packages/client/app/Login.tsx
@@ -1,12 +1,35 @@
 "use client";
 
 import { signIn } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
 
 export function Login() {
+  const imageCount = 8;
   return (
-    <div>
-      <p>Please login!</p>
-      <button onClick={() => signIn("google")}>login with google</button>
+    <div className="h-screen w-full relative flex items-center justify-center bg-base-300">
+      <Image
+        alt="background"
+        src={`/bgs/${Math.ceil(Math.random() * imageCount)}.jpg`}
+        fill={true}
+        style={{ objectFit: "cover" }}
+      />
+
+      <div className="relative bg-base-100 shadow-2xl pt-4 pb-10 px-10 rounded-lg flex flex-col items-center max-w-[400px] w-full ">
+        <Link href={process.env.NEXT_PUBLIC_DOCS_URL || ""}>
+          <Image src="/logo.png" alt="logo" width={200} height={60} />
+        </Link>
+        <div className="h-full flex flex-col space-y-6 items-center">
+          <p className="text-xl font-bold">Welcome back!</p>
+          <button
+            onClick={() => signIn("google")}
+            className="shadow-lg flex space-x-2 items-center  p-2 rounded-lg bg-base-200 w-[300px]"
+          >
+            <Image src="/icons/google.webp" alt="google" width={30} height={30} />
+            <p className="w-full text-center font-medium">Login with Google</p>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -8,5 +8,22 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#570DF8",
+          "secondary": "#F000B8",
+          "accent": "#37CDBE",
+          "neutral": "#3D4451",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 }
