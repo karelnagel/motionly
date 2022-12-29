e2555de076da863a866be30c2de6a2fb1c9b4fd1
Karel <karel789@hotmail.com>
2022-12-29 10:14:19 +0200

    less timeline timestamps

--- packages/client/app/templates/[id]/TimeLine.tsx
+++ packages/client/app/templates/[id]/TimeLine.tsx
@@ -21,16 +21,18 @@ export const Timeline = ({
     <div className="h-full w-full flex flex-col ">
       <div className="h-14 w-full relative p-3">
         <div className=" relative ">
-          {new Array(template.duration * 2 + 1).fill(0).map((_, i) => (
-            <div
-              key={i}
-              className="absolute top-0 flex flex-col -translate-x-1/2 items-center"
-              style={{ left: `${(i / 2 / template.duration) * 100}%` }}
-            >
-              <div className={`h-3  bg-black`} style={{ width: i % 2 === 0 ? 2 : 1 }} />
-              <p className="text-sm">{i % 2 === 0 && Math.floor(i / 2)}</p>
-            </div>
-          ))}
+          {new Array(template.duration * 2 + 1).fill(0).map((_, i) =>
+            i % Math.ceil(template.duration / 10) !== 0 ? null : (
+              <div
+                key={i}
+                className="absolute top-0 flex flex-col -translate-x-1/2 items-center"
+                style={{ left: `${(i / 2 / template.duration) * 100}%` }}
+              >
+                <div className={`h-3  bg-black`} style={{ width: i % 2 === 0 ? 2 : 1 }} />
+                <p className="text-sm">{i % 2 === 0 && Math.floor(i / 2)}</p>
+              </div>
+            )
+          )}
           <div
             className="bg-red-500 h-8 w-1 absolute top-0 -translate-x-1/2 cursor-grab"
             style={{ left: `${(frame / template.fps / template.duration) * 100}%` }}
