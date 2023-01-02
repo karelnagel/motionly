6879fd65d956df991cee5ccc095b3794ec8ae981
Karel <karel789@hotmail.com>
2023-01-02 22:02:54 +0200

    test

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -2,4 +2,3 @@ export * from "./Component";
 export * from "./Composition";
 export * from "./components"
 export * from "./SetSelectedContext";
-export * from "@asius/types/src/components"
--- packages/types/CHANGELOG.md
+++ packages/types/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @asius/types
 
+## 2.0.0
+
+### Major Changes
+
+- Made it compile to js
+
 ## 1.0.1
 
 ### Patch Changes
