16829b972a014a6b0209bcf1e05a16f8deadf65e
Karel <karel789@hotmail.com>
2023-01-16 18:54:52 +0200

    moved timelinebar to separate component

--- packages/client/app/templates/[id]/Timeline/Timeline.tsx
+++ packages/client/app/templates/[id]/Timeline/Timeline.tsx
@@ -11,7 +11,6 @@ export const Timeline = ({
   playerRef: RefObject<PlayerRef>;
 }) => {
   const { template } = useTemplate();
-  const frame = useCurrentPlayerFrame(playerRef);
   const [width, setWidth] = useLocalStorage("timelineWidth", 100);
   return (
     <div className="overflow-x-auto h-full relative">
@@ -19,39 +18,11 @@ export const Timeline = ({
         className="h-full w-full flex flex-col"
         style={{ width: `${width}%` }}
       >
-        <div className="h-14 w-full relative p-3 pr-7">
-          <div className=" relative ">
-            {new Array(template.duration * 2 + 1).fill(0).map((_, i) =>
-              i % Math.ceil(template.duration / 10) !== 0 ? null : (
-                <div
-                  key={i}
-                  className="absolute top-0 flex flex-col -translate-x-1/2 items-center"
-                  style={{
-                    left: `${(i / 2 / template.duration) * 100}%`,
-                  }}
-                >
-                  <div
-                    className={"h-3 bg-base-content"}
-                    style={{ width: i % 2 === 0 ? 2 : 1 }}
-                  />
-                  <p className="text-sm">{i % 2 === 0 && Math.floor(i / 2)}</p>
-                </div>
-              )
-            )}
-            <input
-              id="timeline"
-              type="range"
-              value={frame}
-              onChange={(e) => {
-                playerRef.current?.seekTo(Number(e.currentTarget.value));
-              }}
-              step={1}
-              min={0}
-              max={template.duration * template.fps}
-              className="absolute top-0 left-0 w-full h-6 timeline"
-            />
-          </div>
-        </div>
+        <TimelineBar
+          playerRef={playerRef}
+          duration={template.duration}
+          fps={template.fps}
+        />
         <div className="overflow-y-scroll h-full overflow-x-hidden px-3 pb-2">
           <div className="flex flex-col space-y-2">
             {template.comps.map((comp, i) => (
@@ -74,6 +45,54 @@ export const Timeline = ({
           -
         </button>
         <button onClick={() => setWidth((w) => w * 1.1)}>+</button>
+      </div>
+    </div>
+  );
+};
+
+export const TimelineBar = ({
+  playerRef,
+  duration,
+  fps,
+}: {
+  playerRef: RefObject<PlayerRef>;
+  duration: number;
+  fps: number;
+}) => {
+  const frame = useCurrentPlayerFrame(playerRef);
+
+  return (
+    <div className="h-14 w-full relative p-3 pr-7">
+      <div className="relative">
+        {new Array(duration * 2 + 1).fill(0).map((_, i) =>
+          i % Math.ceil(duration / 10) !== 0 ? null : (
+            <div
+              key={i}
+              className="absolute top-0 flex flex-col -translate-x-1/2 items-center"
+              style={{
+                left: `${(i / 2 / duration) * 100}%`,
+              }}
+            >
+              <div
+                className={"h-3 bg-base-content"}
+                style={{ width: i % 2 === 0 ? 2 : 1 }}
+              />
+              <p className="text-sm">{i % 2 === 0 && Math.floor(i / 2)}</p>
+            </div>
+          )
+        )}
+        <input
+          id="timeline"
+          type="range"
+          value={frame}
+          onChange={(e) => {
+            playerRef.current?.seekTo(Number(e.currentTarget.value));
+          }}
+          step={1}
+          min={0}
+          max={duration * fps}
+          className="absolute top-0 left-0 w-full h-6 timeline"
+        />
       </div>
     </div>
   );
