8bc5973eb9de7c8e22388830ca713b9d1bec951a
Karel <karel789@hotmail.com>
2023-01-31 23:20:35 +0200

    added iframe

---
+++ websites/docs/pages/iframe/index.tsx
@@ -0,0 +1,33 @@
+import { TemplateType } from "@motionly/base";
+import { Player, PlayerProps } from "@motionly/player";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function IFrame() {
+  const [template, setTemplate] = useState<TemplateType>();
+  const router = useRouter();
+  const { autoPlay, controls, clickToPlay, loop } =
+    router.query as Partial<PlayerProps>;
+
+  useEffect(() => {
+    const listener = (event: MessageEvent) => {
+      if (event.data.call === "setTemplate") {
+        setTemplate(event.data.template);
+      }
+    };
+    window.addEventListener("message", listener);
+    return () => window.removeEventListener("message", listener);
+  });
+
+  if (!template) return <div>Loading...</div>;
+  return (
+    <Player
+      style={{ width: "100%" }}
+      template={template}
+      autoPlay={!!autoPlay}
+      loop={!!loop}
+      controls={!!controls}
+      clickToPlay={!!clickToPlay}
+    />
+  );
+}
