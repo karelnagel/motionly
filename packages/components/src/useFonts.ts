2bda6c3fb9add96c37566ca6003a68e8f70fa650
Karel <karel789@hotmail.com>
2023-01-31 23:43:40 +0200

    fixed font loading

--- packages/components/src/useFonts.ts
+++ packages/components/src/useFonts.ts
@@ -2,31 +2,36 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { continueRender, delayRender } from "remotion";
 
+const loadFonts = async (fonts: string[]) => {
+  if (fonts.length > 0) {
+    try {
+      const url = `https://fonts.googleapis.com/css?family=${fonts.join("|")}`;
+      await axios.get(url);
+
+      const link = document.createElement("link");
+      link.href = url;
+      link.rel = "stylesheet";
+      link.type = "text/css";
+      document.head.appendChild(link);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+};
+
 export const useFonts = async (fonts: string[] = []) => {
   const [handle] = useState(() => delayRender("Loading fonts"));
-  const [loaded, setLoaded] = useState(false);
+  const [loadedFonts, setLoadedFonts] = useState([] as string[]);
 
   useEffect(() => {
-    const load = async () => {
-      if (fonts.length > 0) {
-        try {
-          const url = `https://fonts.googleapis.com/css?family=${fonts.join(
-            "|"
-          )}`;
-          await axios.get(url);
-
-          const link = document.createElement("link");
-          link.href = url;
-          link.rel = "stylesheet";
-          link.type = "text/css";
-          document.head.appendChild(link);
-        } catch (e) {
-          console.log(e);
-        }
-      }
-      continueRender(handle);
-      setLoaded(true);
-    };
-    if (!loaded) load();
+    const fontsToLoad = fonts.filter((f) => !loadedFonts.includes(f));
+    if (fontsToLoad?.length) {
+      loadFonts(fonts)
+        .then(() => {
+          setLoadedFonts((f) => [...f, ...fontsToLoad]);
+          continueRender(handle);
+        })
+        .catch((e) => console.log(e));
+    }
   }, [fonts]);
 };
