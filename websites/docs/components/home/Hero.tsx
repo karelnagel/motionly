0688c6178d0130e0e6b921774890cc6daaa8eadc
Karel <karel789@hotmail.com>
2023-02-01 09:17:30 +0200

    added image priority

--- websites/docs/components/home/Hero.tsx
+++ websites/docs/components/home/Hero.tsx
@@ -8,18 +8,25 @@ export const Hero = () => {
   return (
     <div className="relative flex items-center justify-between px-4 flex-col-reverse md:flex-row my-10 md:my-32">
       <div className="flex flex-col space-y-6 items-center md:items-start  max-w-lg">
-        <h1 className="title font-semibold uppercase leading-[1] text-[50px] md:text-[85px] tracking-wide
-        animate-slide-from-left-600">
+        <h1
+          className="title font-semibold uppercase leading-[1] text-[50px] md:text-[85px] tracking-wide
+        animate-slide-from-left-600"
+        >
           Automate your content
         </h1>
-        <p className="text-[22px] leading-[30px] font-light
-        animate-slide-from-left-700">
+        <p
+          className="text-[22px] leading-[30px] font-light
+        animate-slide-from-left-700"
+        >
           Automate your video editing and integrate
           <br /> videos with dynamic data on your site.
         </p>
         <Link href="/#contact">
-          <Button text="Get started" className="text-[24px] font-normal p-3 pl-6
-        animate-slide-from-left-700"/>
+          <Button
+            text="Get started"
+            className="text-[24px] font-normal p-3 pl-6
+        animate-slide-from-left-700"
+          />
         </Link>
       </div>
       <div>
@@ -44,9 +51,13 @@ export const Hero = () => {
               loop
               muted
             />
-            <Image src={laptop} alt="laptop" className="relative " />
+            <Image
+              priority
+              src={laptop}
+              alt="laptop"
+              className="relative "
+            />
           </div>
-
         </div>
       </div>
     </div>
