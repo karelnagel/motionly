5eae21ce90303f73138c68c259ded707a6ce0c8c
Karel <karel789@hotmail.com>
2023-02-17 18:10:58 +0200

    login redirect

--- websites/client/app/(layout)/login/page.tsx
+++ websites/client/app/(layout)/login/page.tsx
@@ -4,26 +4,33 @@ import { signIn } from "next-auth/react";
 import google from "../../../public/icons/google.png";
 import github from "../../../public/icons/github.png";
 import Image, { StaticImageData } from "next/image";
-const Button = ({
-  service,
-  src,
+
+export default function Login({
+  searchParams: { redirect },
 }: {
-  service: string;
-  src: StaticImageData;
-}) => {
-  return (
-    <button
-      onClick={() =>
-        signIn(service.toLowerCase(), {  callbackUrl: "/templates" })
-      }
-      className="btn flex space-x-2"
-    >
-      <Image src={src} alt={service} className="w-6" />
-      <p>Continue with {service}</p>
-    </button>
-  );
-};
-export default function Login() {
+  searchParams: { redirect: string };
+}) {
+  const Button = ({
+    service,
+    src,
+  }: {
+    service: string;
+    src: StaticImageData;
+  }) => {
+    return (
+      <button
+        onClick={() =>
+          signIn(service.toLowerCase(), {
+            callbackUrl: redirect || "/templates",
+          })
+        }
+        className="btn flex space-x-2"
+      >
+        <Image src={src} alt={service} className="w-6" />
+        <p>Continue with {service}</p>
+      </button>
+    );
+  };
   return (
     <div className="h-full flex justify-center items-center flex-grow">
       <div className="flex flex-col items-center justify-center">
--- websites/client/components/Navbar.tsx
+++ websites/client/components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Login } from "./Login";
+import { Login } from "./LoginButton";
 import { Logo } from "./Logo";
 
 const items: { name: string; route: string }[] = [
