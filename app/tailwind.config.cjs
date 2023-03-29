7c0087fb3a4a21f4a4ced487dad10ea2b10eeb71
Karel Nagel <nagelkarel@gmail.com>
2023-03-29 20:17:17 +0300

    tailwind and daisy

---
+++ app/tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+  },
+  plugins: [require("daisyui")],
+}
