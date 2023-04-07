75eaf6a134889848e077eb72d71fb7a107e48f34
Karel Nagel <nagelkarel@gmail.com>
2023-04-07 11:24:22 +0300

    preview and rendering working

--- packages/composition/src/root.tsx
+++ packages/composition/src/root.tsx
@@ -4,20 +4,61 @@ import { Composition as Comp } from "./Composition";
 import { LoadFonts } from "./helpers/LoadFonts";
 import { Template } from "./types";
 
-const inputProps = getInputProps() as Template;
+const defaultProps = (error: string): Template => ({
+  allComponents: ["1"],
+  components: {
+    "1": {
+      duration: 3,
+      from: 0,
+      height: 1080,
+      width: 1920,
+      id: "1",
+      opacity: 1,
+      rotation: 0,
+      type: "text",
+      wrappers: {
+        animation: {
+          type: "animation",
+          animations: [{ id: "as", prop: "translateY", type: "interpolate", start: 0, duration: 3, from: 0, to: -3000 }],
+        },
+      },
+      x: 0,
+      y: 0,
+      props: {
+        text: `Rendering is working, but you sent invalid template!\nHere's the error:\n ${error}`,
+        textStyle: {
+          fontFamily: "Roboto",
+          color: "#000000",
+          background: "#00ff00",
+          fontSize: 40,
+          fontWeight: "800",
+        },
+      },
+    },
+  },
+  duration: 3,
+  fps: 30,
+  height: 1080,
+  width: 1920,
+  id: "Main",
+  name: "Main",
+  background: "#ffffff",
+});
 
+const inputProps = Template.safeParse(getInputProps());
+const template = inputProps.success ? inputProps.data : defaultProps(inputProps.error.message);
 export const Root: React.FC = () => {
   return (
     <>
-      <LoadFonts template={inputProps} />
+      <LoadFonts template={template} />
       <Composition
         id="Main"
         component={Comp}
-        durationInFrames={Math.round((inputProps.duration || 1) * inputProps.fps)}
-        fps={inputProps.fps}
-        width={inputProps.width}
-        height={inputProps.height}
-        defaultProps={{ template: inputProps }}
+        durationInFrames={Math.round((template.duration || 1) * template.fps)}
+        fps={template.fps}
+        width={template.width}
+        height={template.height}
+        defaultProps={{ template }}
       />
     </>
   );
